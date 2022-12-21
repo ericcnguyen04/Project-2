de1ff07ac297191ff4273c4c08085afaa47be26c
ericcnguyen04 <ericc.win@gmail.com>
2022-12-21 15:34:39 -0500

    lunch

--- controllers/users.js
+++ controllers/users.js
@@ -27,10 +27,51 @@ router.post('/', async (req, res) => {
         res.cookie('userID', newUser.id)
         // redirect to the home page (for now)
         res.redirect('/')
+    } catch(err) {x
+        console.log(err)
+        res.status(500).send('server error')
+    }
+})
+
+// GET /users/login -- render a login form that POSTs to /users/login
+router.get('/login', (req, res) => {
+    res.render('users/login.ejs', {
+        message: req.query.message ? req.query.message : null
+    })
+})
+
+// POST /users/login -- ingest data from form rendered @ GET /users/login
+router.post('/login', async (req,res) => {
+    try {
+        // look up the user based on their email
+        const user = await db.user.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        // booilerplate message if login fails
+        const badCredentialMessage = 'username or password incorrect'
+        if (!user) {
+            // if the user isn't found in the db
+            res.redirect('/users/login?message=' + badCredentialMessage)
+        } else if (user.password !== req.body.password) {
+            // if the user's supplied password is incorrect
+            res.redirect('/users/login?message=' + badCredentialMessage)
+        } else {
+            // if the user is found and their password matches. log them in
+            console.log('logging user in!')
+            res.cookie('userID', user.id)
+            res.redirect('/')
+        }
     } catch(err) {
         console.log(err)
         res.status(500).send('server error')
     }
+})
+
+// GET /users/logout -- clear any cookies and redirect to the homepage
+router.get('/logout', (req,res) => {
+    res.send('log the user out by clearing the cookie')
 })
 
 // export the router
